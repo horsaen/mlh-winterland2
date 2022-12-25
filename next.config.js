0261cd2c96ececc056d09b835a186055dca892e4
horsaen <atealltheglue@protonmail.com>
2022-12-25 02:53:51 -0600

    allow for external img

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  images: {
+    domains: ['edamam-product-images.s3.amazonaws.com']
+  }
 }
-
-module.exports = nextConfig
